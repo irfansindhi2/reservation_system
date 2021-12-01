33cef16988f9d6000f252b0a3adbd85440c60a8c
Irfan Sindhi <irfansindhi2@gmail.com>
2021-12-01 15:57:57 -0600

    Add files via upload

--- reserve.php
+++ reserve.php
@@ -18,38 +18,159 @@ if(isset($_POST['submit'])){
   
 <head>
     <title>Reservation System</title>
+
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<style>
+@import url(https://fonts.googleapis.com/css?family=Open+Sans:400italic,400,300,600);
+
+* {
+	margin:0;
+	padding:0;
+	box-sizing:border-box;
+	-webkit-box-sizing:border-box;
+	-moz-box-sizing:border-box;
+	-webkit-font-smoothing:antialiased;
+	-moz-font-smoothing:antialiased;
+	-o-font-smoothing:antialiased;
+	font-smoothing:antialiased;
+	text-rendering:optimizeLegibility;
+}
+
+body {
+	font-family:"Open Sans", Helvetica, Arial, sans-serif;
+	font-weight:300;
+	font-size: 12px;
+	line-height:30px;
+	color:#777;
+	background:#0CF;
+}
+
+.container {
+	max-width:400px;
+	width:100%;
+	margin:0 auto;
+	position:relative;
+}
+
+#contact input[type="text"], #contact input[type="email"], #contact input[type="tel"], #contact input[type="datetime-local"], #contact input[type="number"], #contact button[type="submit"] { font:400 12px/16px "Open Sans", Helvetica, Arial, sans-serif; }
+
+#contact {
+	background:#F9F9F9;
+	padding:25px;
+	margin:50px 0;
+}
+
+#contact h3 {
+	color: #F96;
+	display: block;
+	font-size: 30px;
+	font-weight: 400;
+}
+
+#contact h4 {
+	margin:5px 0 15px;
+	display:block;
+	font-size:13px;
+}
+
+fieldset {
+	border: medium none !important;
+	margin: 0 0 10px;
+	min-width: 100%;
+	padding: 0;
+	width: 100%;
+}
+
+#contact input[type="text"], #contact input[type="email"], #contact input[type="tel"], #contact input[type="datetime-local"], #contact input[type="number"] {
+	width:100%;
+	border:1px solid #CCC;
+	background:#FFF;
+	margin:0 0 5px;
+	padding:10px;
+}
+
+#contact input[type="text"]:hover, #contact input[type="email"]:hover, #contact input[type="tel"]:hover, #contact input[type="datetime-local"]:hover, #contact input[type="number"] {
+	-webkit-transition:border-color 0.3s ease-in-out;
+	-moz-transition:border-color 0.3s ease-in-out;
+	transition:border-color 0.3s ease-in-out;
+	border:1px solid #AAA;
+}
+
+#contact textarea {
+	height:100px;
+	max-width:100%;
+  resize:none;
+}
+
+#contact button[type="submit"] {
+	cursor:pointer;
+	width:100%;
+	border:none;
+	background:#0CF;
+	color:#FFF;
+	margin:0 0 5px;
+	padding:10px;
+	font-size:15px;
+}
+
+#contact button[type="submit"]:hover {
+	background:#09C;
+	-webkit-transition:background 0.3s ease-in-out;
+	-moz-transition:background 0.3s ease-in-out;
+	transition:background-color 0.3s ease-in-out;
+}
+
+#contact button[type="submit"]:active { box-shadow:inset 0 1px 3px rgba(0, 0, 0, 0.5); }
+
+#contact input:focus, #contact textarea:focus {
+	outline:0;
+	border:1px solid #999;
+}
+::-webkit-input-placeholder {
+ color:#888;
+}
+:-moz-placeholder {
+ color:#888;
+}
+::-moz-placeholder {
+ color:#888;
+}
+:-ms-input-placeholder {
+ color:#888;
+}
+
+        </style>
 </head>
 
 <body>
-    <center>
-        <h1>Reservations Here!!!</h1>
+
+<div class="container">  
+  <form id="contact" action="" method="post">
+    <h3>Book Your Table</h3>
+    <h4>Quick and Easy!</h4>
+    <fieldset>
+      <input id="fname" name="fname" placeholder="Your name" type="text" tabindex="1" required autofocus>
+    </fieldset>
+    <fieldset>
+      <input id="email" name="email" placeholder="Your Email Address" type="email" tabindex="2" required>
+    </fieldset>
+    <fieldset>
+      <input id="phone" name="phone" placeholder="Your Phone Number" type="tel" tabindex="3" required>
+    </fieldset>
+    <fieldset>
+      <input id="date" name="date" placeholder="Reservation Date and Time" class="form-control" type="text" onfocus="(this.type='datetime-local')" onblur="(this.type='text')" required tabindex="5">
+    </fieldset>
+    <fieldset>
+    <input id="guests" name="guests" min=1 type = "number" placeholder="Number of Guests " tabindex="5" required>
+    </fieldset>
+    <fieldset>
+      <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+    </fieldset>
+  </form>
+ 
   
-        <form action="" method="post">
-              
-            <p>
-                <label for="fname">Name: </label>
-                <input type="text" id="fname" name="fname" required>
-            </p>
-            <p>
-                <label for="phone">Phone: </label>
-                <input type="tel" id="phone" name="phone" placeholder="1234567890" pattern="[0-9]{10}" required>
-            </p>
-            <p>
-                <label for="email">Email: </label>
-                <input type="email" id="email" name="email" required>
-            </p>
-            <p>
-                <label for="date">Date and Time: </label>
-                <input type="datetime-local" id="date" name="date" required>
-            </p>  
-            <p>
-                <label for="guests">Number of Guests: </label>
-                <input type="number" id="guests" name="guests" min=1 required>
-            </p>
-              
-            <input type="submit" name="submit" value="Submit">
-        </form>
-    </center>
+</div>
 </body>
   
 </html>
