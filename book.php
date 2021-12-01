7b48b491e93a42239019f391cfe3e59c04580800
Irfan Sindhi <irfansindhi2@gmail.com>
2021-12-01 17:24:46 -0600

    Add files via upload

--- book.php
+++ book.php
@@ -7,9 +7,18 @@ require 'header.php';
   
 <head>
     <title>Book Tables</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/form.css">
 </head>
 <body>
-
+<div class="container">  
+  <form id="contact" action="check.php" method="post">
+  <h3>Available Tables</h3>
+    <table>
+    <tr>
+    <th>Capacity</th>
+    <th>Reserve</th>
+    </tr>
 <?php
 // var_dump($_SESSION);
 
@@ -17,36 +26,45 @@ if(isset($_SESSION["error"])){
     $error = $_SESSION["error"];
     echo "<span>$error</span>";
 }
-?> 
-<form action="check.php" method="post">
 
-<?php
 //var_dump($_SESSION['post-data']);
 $sql = "SELECT table_id, table_capacity, table_occupied FROM table_info";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) { 
   
-  $tableid = array();
-  $table_capacity = array();
-  $table_occupied = array();
-  while($row = $result->fetch_assoc()) {   
-    echo "table_id: " . $row["table_id"]. " table_capacity: " . $row["table_capacity"];
+  while($row = $result->fetch_assoc()) {  
+       
+    echo "<tr><th>" . $row["table_capacity"] . "</th>";
 
     if($row["table_occupied"] == 0){
-        echo " table_occupied <a>" . $row["table_occupied"] . "</a>";
-        echo " <input type=\"checkbox\" name=\"check_list[]\" value=". $row["table_id"] . "><br>";
+        echo "<th><input type=\"checkbox\" name=\"check_list[]\" value=". $row["table_id"] . "></fieldset><th>";
+        echo "</tr>";
     }
     else{
-        echo " table_occupied " . $row["table_occupied"]. "<br>";
+        echo "<th color = 'red'>Occupied</fieldset><th></tr>";
     }
 }
-} else {
-  echo "0 results";
 }
-$conn->close();
 ?>
-<input type="submit" value="Submit">
+
+</table>
+<fieldset>
+      <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+</fieldset>
+
 <a href="reserve.php">Book Again</a>
 </form>
+</div>
+
+
+
+
+
+
+
 </body>
+
+
+
+</html>
