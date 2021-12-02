d51c0ac7eb6249a567e9b7c1d0cacb26fe0f913e
Irfan Sindhi <irfansindhi2@gmail.com>
2021-12-02 03:56:39 -0600

    Add files via upload

--- reservation_system.sql
+++ reservation_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 30, 2021 at 11:06 PM
+-- Generation Time: Dec 02, 2021 at 10:56 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 7.3.31
 
@@ -41,8 +41,10 @@ CREATE TABLE `reservation_log` (
 --
 
 INSERT INTO `reservation_log` (`reserve_id`, `user_id`, `startdatetime`, `enddatetime`, `guest_num`, `table_ids`) VALUES
-(25, NULL, '2022-06-29 17:00:00', NULL, 6, '1,2'),
-(26, NULL, '2021-11-10 17:02:00', NULL, 8, '4');
+(35, NULL, '2021-12-01 18:28:00', '2021-12-01 19:28:00', 6, '1,2'),
+(36, NULL, '2021-12-16 01:29:00', '2021-12-16 02:29:00', 6, '3'),
+(37, NULL, '2021-12-02 03:42:00', '2021-12-02 04:42:00', 6, '1,2'),
+(38, NULL, '2021-12-02 03:47:00', '2021-12-02 04:47:00', 6, '3');
 
 -- --------------------------------------------------------
 
@@ -52,20 +54,19 @@ INSERT INTO `reservation_log` (`reserve_id`, `user_id`, `startdatetime`, `enddat
 
 CREATE TABLE `table_info` (
   `table_id` int(255) NOT NULL,
-  `table_capacity` int(255) DEFAULT NULL,
-  `table_occupied` int(255) DEFAULT NULL
+  `table_capacity` int(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `table_info`
 --
 
-INSERT INTO `table_info` (`table_id`, `table_capacity`, `table_occupied`) VALUES
-(1, 2, 1),
-(2, 4, 1),
-(3, 6, 0),
-(4, 8, 1),
-(5, 2, 0);
+INSERT INTO `table_info` (`table_id`, `table_capacity`) VALUES
+(1, 2),
+(2, 4),
+(3, 6),
+(4, 8),
+(5, 2);
 
 -- --------------------------------------------------------
 
@@ -77,17 +78,25 @@ CREATE TABLE `users` (
   `user_id` int(255) NOT NULL,
   `user_name` varchar(255) DEFAULT NULL,
   `user_pass` varchar(255) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
+  `fname` varchar(255) DEFAULT NULL,
   `phone` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL
+  `email` varchar(255) DEFAULT NULL,
+  `mail_address` varchar(25) NOT NULL,
+  `bill_address` varchar(255) NOT NULL,
+  `pref_payment` varchar(255) NOT NULL,
+  `reward` float NOT NULL,
+  `pref_diner` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `user_name`, `user_pass`, `name`, `phone`, `email`) VALUES
-(1, 'irfan', 'a', 'Irfan', '1234567890', 'irfansindhi2@gmail.com');
+INSERT INTO `users` (`user_id`, `user_name`, `user_pass`, `fname`, `phone`, `email`, `mail_address`, `bill_address`, `pref_payment`, `reward`, `pref_diner`) VALUES
+(1, 'irfan', 'a', 'Irfan', '1234567890', 'irfansindhi2@gmail.com', '', '', '', 0, ''),
+(3, 'Irfana', 'a', 'Irfan', '1234567890', 'irfansindhi21@gmail.com', 'asdad', 'asdad', 'cash', 0, ''),
+(4, 'Irfanaa', 'a', 'Irfan', '1234567890', 'irfansindhi221@gmail.com', 'asdad', 'asdad', 'cash', 0, ''),
+(5, 'Irfann', 'a', 'Irfan', '1234567890', 'irfansindhi23@gmail.com', 'asdad', 'asdad', 'cash', 0, '');
 
 --
 -- Indexes for dumped tables
@@ -119,7 +128,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `reservation_log`
 --
 ALTER TABLE `reservation_log`
-  MODIFY `reserve_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `reserve_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- AUTO_INCREMENT for table `table_info`
@@ -131,7 +140,7 @@ ALTER TABLE `table_info`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `user_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
