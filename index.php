0dad263d0956f71b03fe9881926114c826e8f034
Irfan Sindhi <irfansindhi2@gmail.com>
2021-12-03 13:04:26 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 
-	<title>Pizza - free responsive website template</title>
+	<title>Take A Byte</title>
 	<meta name="keywords" content="">
 	<meta name="description" content="">
     <meta name="author" content="templatemo">
@@ -38,7 +38,7 @@
 					<span class="icon icon-bar"></span>
 					<span class="icon icon-bar"></span>
 				</button>
-				<a href="#home" class="navbar-brand smoothScroll"><strong>PIZZA</strong></a>
+				<a href="#home" class="navbar-brand smoothScroll"><strong>Take A Byte</strong></a>
 			</div>
 			<div class="collapse navbar-collapse">
 				<ul class="nav navbar-nav navbar-right">
@@ -58,9 +58,9 @@
 			<li>
 				<img src="images/slider-img1.jpg" alt="Pizza Image 1">
 				<div class="flex-caption">
-					<h2 class="slider-title">We make Pizza</h2>
+					<h2 class="slider-title">We make Pizza and have internet!</h2>
 					<h3 class="slider-subtitle">Fresh, clean, and delicious.</h3>
-					<p class="slider-description">Praesent tincidunt neque semper elementum gravida. Donec id euismod magna. Ut erat ligula, malesuada eu quam a, fringilla auctor augue.</p>
+					<p class="slider-description">Take A Byte is your one stop shop for your favorite pizza, beverages, and atmosphere. We have a large menu of food and drinks and have internet and video games for your entertainment needs!</p>
 				</div>
 			</li>
 			<li>
@@ -80,13 +80,13 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-					<h1 class="text-uppercase">Your Pizza Shop</h1>
+					<h1 class="text-uppercase">Welcome to Take A Byte!</h1>
 				</div>
 				<div class="col-md-6 col-sm-6">					
-					<h3 class="text-uppercase padding-bottom-10">Another Baker</h3>
+					<h3 class="text-uppercase padding-bottom-10">Your favorite pizza shop/internet cafe!</h3>
+                    <p>Take A Byte is a brand new pizza place/internet cafe. We have tons of cool stuff here like pizza, internet, beer, video games, ice cream, and a great atmosphere.</p>
+					<p>Take A Byte is not only the world's best new pizza shop/internet cafe, it's also the new home for your Fun Friday evenings with happy hour starting at 5 pm and video game tournmanets throughout the weekend.</p>
 					<p>Pizza responsive web template is provided by <a rel="nofollow" href="http://www.templatemo.com" target="_parent">templatemo</a> website. Feel free to download, adapt, and use this template for your websites. Credit goes to <a rel="nofollow" href="http://pixabay.com" target="_parent">Pixabay</a> for images used in this template.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet. Dolore magna aliquam erat volutpat.</p>
-					<p>Proin enim sem, ultricies sit amet convallis nec, sodales quis augue. Duis consequat felis ac justo luctus, a cursus tellus pharetra. In ullamcorper gravida enim id pulvinar.</p>
 				</div>
 				<div class="col-md-6 col-sm-6">
 					<img src="images/about-img1.jpg" class="img-responsive img-bordered img-about" alt="about img">
@@ -108,8 +108,8 @@
 					<div class="gallery-wrapper">
 						<img src="images/gallery-img1.jpg" class="img-responsive gallery-img" alt="Pizza 1">
 						<div class="gallery-des">
-							<h3>Curabitur </h3>
-							<h5>Cras in ante mattis, elementum nunc sed.</h5>
+							<h3>Personal Pizzas </h3>
+							<h5>Our Personal size pizzas are made to order and are never frozen or microwaved</h5>
 						</div>
 					</div>
 				</div>	
@@ -117,8 +117,8 @@
 					<div class="gallery-wrapper">
 						<img src="images/gallery-img2.jpg" class="img-responsive gallery-img" alt="Pizza 2">
 						<div class="gallery-des">
-							<h3>Lorem ipsum</h3>
-							<h5>In ullamcorper gravida enim id pulvinar</h5>
+							<h3>Dining Services</h3>
+							<h5>We offer both indoor and outdoor dining options</h5>
 						</div>
 					</div>
 				</div>
@@ -126,8 +126,8 @@
 					<div class="gallery-wrapper">
 						<img src="images/gallery-img3.jpg" class="img-responsive gallery-img" alt="Pizza 3">
 						<div class="gallery-des">
-							<h3>Pellentesque</h3>
-							<h5>Maecenas efficitur nisi id sapien</h5>
+							<h3>Beverages</h3>
+							<h5>To go along with our world class pizza we also offer a wide range of both alcoholic and non alcoholic drinks</h5>
 						</div>
 					</div>
 				</div>
@@ -135,8 +135,8 @@
 					<div class="gallery-wrapper">
 						<img src="images/gallery-img4.jpg" class="img-responsive gallery-img" alt="Pizza 4">
 						<div class="gallery-des">
-							<h3>Suspendisse</h3>
-							<h5>Mauris sit amet augue sit amet risus</h5>
+							<h3>Pizza Toppings</h3>
+							<h5>We have tons of different pizza toppings to add to your favorite slice of pizza</h5>
 						</div>
 					</div>
 				</div>
@@ -144,8 +144,8 @@
 					<div class="gallery-wrapper">
 						<img src="images/gallery-img5.jpg" class="img-responsive gallery-img" alt="Pizza 5">
 						<div class="gallery-des">
-							<h3>Elementum</h3>
-							<h5>Maecenas efficitur nisi id sapien</h5>
+							<h3>Reserve a table!</h3>
+							<h5>With our online reservation system you can easily reserve a table instead of waiting on line</h5>
 						</div>
 					</div>
 				</div>				
@@ -188,10 +188,10 @@
 						<a href="tel:080-090-0660" class="contact-link">080-090-0660</a>
 					</p>			
 					<p><i class="fa fa-envelope-o contact-fa"></i> 
-                    	<a href="mailto:hello@company.com" class="contact-link">hello@company.com</a></p>
+                    	<a href="mailto:hello@company.com" class="contact-link">TakeAByte@byte.com</a></p>
 				</div>
 				<div class="col-md-4 col-sm-4">
-					<h3 class="text-uppercase">Opening hours</h3>
+					<h3 class="text-uppercase">Operating Hours:</h3>
 					<p><i class="fa fa-clock-o contact-fa"></i> 7:00 AM - 11:00 PM</p>
 					<p><i class="fa fa-bell-o contact-fa"></i> Monday to Friday and Sunday</p>
                     <p><i class="fa fa-download contact-fa"></i> 
@@ -213,7 +213,7 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12">
-					<p>Copyright &copy; 2084 Company Name</p>
+					<p>Copyright &copy; 2084 Take A Byte</p>
 					<hr>
 					<ul class="social-icon">
 						<li><a href="#" class="fa fa-facebook"></a></li>
